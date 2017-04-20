30e26bb1666dcce6772a4d73c893b6250854bc30
André Secco <andreluizsecco@yahoo.com.br>
2017-04-20 20:57:31 -0300

    Initial commit

---
+++ AspnetMVC/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using AspnetMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AspnetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Livro> Livros;
+
+        public HomeController()
+        {
+            if(Livros == null)
+                Livros = new Livro().GetLivros();
+        }
+
+        public ActionResult Index()
+        {
+            return View(Livros.OrderBy(x => x.ID));
+        }
+
+        public ActionResult NovoLivro()
+        {
+            return View();
+        }
+
+        public ActionResult EditarLivro(int id)
+        {
+            return View(Livros.FirstOrDefault(x => x.ID == id));
+        }
+
+        [HttpPost]
+        public ActionResult InserirLivro(Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!Livros.Any(x => x.ID == livro.ID))
+                    Livros.Add(livro);
+                return RedirectToAction("Index");
+            }
+            else
+                return View("NovoLivro", livro);
+        }
+
+        [HttpPost]
+        public ActionResult AtualizarLivro(Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                var index = Livros.FindIndex(x => x.ID == livro.ID);
+                Livros[index] = livro;
+                return RedirectToAction("Index");
+            }
+            else
+                return View("EditarLivro", livro);
+        }
+        
+        public ActionResult ExcluirLivro(int id)
+        {
+            Livros.Remove(Livros.FirstOrDefault(x => x.ID == id));
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AspnetMVC/Models/Livro.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspnetMVC.Models
+{
+    public class Livro
+    {
+        [Required(ErrorMessage = "O Código é obrigatório!")]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "O Título é obrigatório!")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Autor é obrigatório!")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "O Ano de publicação é obrigatório!")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "O Ano deve possuir 4 números!")]
+        public int AnoPublicacao { get; set; }
+
+        public List<Livro> GetLivros()
+        {
+            return new List<Livro>
+            {
+                new Livro
+                {
+                    ID = 1,
+                    Titulo = "Domain-Driven Design: Tackling Complexity in the Heart of Software",
+                    Autor = "Eric Evans",
+                    AnoPublicacao = 2003
+                },
+                new Livro
+                {
+                    ID = 2,
+                    Titulo = "Agile Principles, Patterns, and Practices in C#",
+                    Autor = "Robert C. Martin",
+                    AnoPublicacao = 2006
+                },
+                new Livro
+                {
+                    ID = 3,
+                    Titulo = "Clean Code: A Handbook of Agile Software Craftsmanship",
+                    Autor = "Robert C. Martin",
+                    AnoPublicacao = 2008
+                }
+            };
+        }
+    }
+}
